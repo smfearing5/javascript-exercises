3a2d5ac1143dde93dc0199439043c09ed886f45c
Steven Fearing <smfearing5@gmail.com>
2023-04-09 17:37:13 -0400

    Complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let output = 0;
+
+  for (i=0; i<arr.length; i++) output += arr[i];
+
+  return output;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let output = arr[0];
 
+  for (i=1; i<arr.length; i++) output *= arr[i];
+
+  return output;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0 || num === 1) return 1;
+
+  let output = 1;
+
+  for (i=2; i<=num; i++) {
+    output *= i;
+  }
+
+  return output;
 };
 
 // Do not edit below this line
